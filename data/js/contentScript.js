dd66e3db49a088ffe3c6ddde81c76189db4c7eaa
viraniac <viraniac@gmail.com>
2015-05-10 17:45:01 +0530

    - Added code in content script to detect and enter language. - Removed code from main.js that was there to handle spell check dictionary   Apparently we don't have to deal with dictionaries after all.

---
+++ data/js/contentScript.js
@@ -0,0 +1,33 @@
+var minimum_character_length = 25;
+
+function handleKeyDown(evt) {
+    var key = evt.keyCode || evt.charCode;
+
+    // check if user has finished entering a word
+    if (key == 32) {
+        // we are only going to check language if there is some amount of text available as
+        // that will increase our chances of detecting language correctly.
+        if (evt.target.value.length > minimum_character_length) {
+            // Looks like we have enough text to reliably detect the language.
+            guessLanguage.detect(evt.target.value, function(language) {
+
+                // lets set the language as the one that we have detected
+                evt.target.lang = language;
+
+                // now lets reset the spell checker so that it will check based on the detected language.
+                evt.target.spellcheck = false;
+                evt.target.spellcheck = true;
+            });
+        }
+    }
+}
+
+function initialize(){
+    var textareas = document.querySelectorAll("textarea");
+    
+    for (var i = 0; i < textareas.length; i++ ) {
+        textareas[i].addEventListener('keydown', handleKeyDown);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', initialize);

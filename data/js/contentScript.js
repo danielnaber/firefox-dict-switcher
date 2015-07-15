908a97e4a871a713a5a98989fecbdfd0020841e0
Daniel Naber <naber@danielnaber.de>
2015-07-15 14:48:58 +0200

    increase refresh rate

--- data/js/contentScript.js
+++ data/js/contentScript.js
@@ -14,7 +14,7 @@
 //   limitations under the License.
 
 const minimumCharacterLength = 25,
-      feedBackDivRefreshRate = 100;
+      feedBackDivRefreshRate = 250;
       feedBackWaitText = "...";
 
 let userPreferences,

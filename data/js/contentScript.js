1aa3eec34786edbbf19cf10680c34fd691ef22a8
Daniel Naber <naber@danielnaber.de>
2015-10-31 17:03:33 +0100

    if minimum number of chars is not (yet) reached, disable spell checking for that element - that's what the previous release did

--- data/js/contentScript.js
+++ data/js/contentScript.js
@@ -5,6 +5,39 @@
 // If a copy of the MPL was not distributed with this file, You can obtain one at
 // http://mozilla.org/MPL/2.0/.
 
+var currentTarget;
+
+self.port.on("setSpellChecking", setSpellChecking);
+
+function setSpellChecking(spellChecking) {
+    if (!currentTarget) {
+        // shouldn't happen
+        console.log("currentTarget is not set");
+        return;
+    }
+    // If spell checking was disabled by the page developer, we honor the setting and don't set the value of
+    // the spellcheck attribute to true, but we still try to detect the language and keep the ordinary operation
+    // of the addon because the user can manually enable spell checking from the context menu, and the spellcheck
+    // attribute stays false.
+    // I set the developerDisabledSpellChecking to indicate this case
+    // (see how the firefoxDictSwitcherDisabledSpellCheck flag is set later)
+    const developerDisabledSpellChecking = currentTarget.spellcheck === false &&
+                                          !currentTarget.dataset.firefoxDictSwitcherDisabledSpellCheck;
+    if (spellChecking) {
+        if(!developerDisabledSpellChecking) {
+            currentTarget.spellcheck = true;
+        }
+    } else {
+        // Because we can't detect the language (yet), disable spell checking
+        currentTarget.spellcheck = false;
+        // And set a flag to indicate that it's our code who disabled spell checking not the page developer's
+        // We do so only if the attribute wasn't already set to false be the page developer
+        if(!developerDisabledSpellChecking) {
+            currentTarget.dataset.firefoxDictSwitcherDisabledSpellCheck = "1";
+        }
+    }
+}
+
 function isEligible(element) {
     // The addon operates only on textarea elements or elements with the contentEditable attribute set.
     return element.tagName == "TEXTAREA" || element.isContentEditable;
@@ -15,6 +48,7 @@ document.documentElement.addEventListener("keydown", function (evt) {
         var key = evt.keyCode || evt.charCode;
         // check if user has finished entering a word
         if(key == 32/*space*/ || key == 188/*comma*/ || key == 190/*dot*/) {
+            currentTarget = evt.target;
             let text = evt.target.value || evt.target.textContent;
             self.port.emit("detectLanguage", text);
         }
@@ -27,6 +61,7 @@ document.documentElement.addEventListener("keydown", function (evt) {
 document.documentElement.addEventListener("focus", function (evt) {
     // Set correct language when we set the focus to already filled textarea
     if(isEligible(evt.target)) {
+        currentTarget = evt.target;
         let text = evt.target.value || evt.target.textContent;
         self.port.emit("detectLanguage", text);
     }
@@ -37,6 +72,7 @@ document.documentElement.addEventListener("paste", function (e) {
         // We try to detect only if the pasted data is available in plain text format
         let text = e.clipboardData.getData("text/plain");
         if(text) {
+            currentTarget = e.target;
             self.port.emit("detectLanguage", text);
         }
     }

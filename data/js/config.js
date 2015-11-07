5f0fb6153ffb97516f958f4abff27c92e788c9be
Daniel Naber <naber@danielnaber.de>
2015-11-07 18:43:54 +0100

    add comment

--- data/js/config.js
+++ data/js/config.js
@@ -1,4 +1,5 @@
 
+// open the add-ons configuration (from http://stackoverflow.com/questions/22593454/):
 AddonManager.getAddonByID(self.options.id, function(aAddon) {
     unsafeWindow.gViewController.commands.cmd_showItemDetails.doCommand(aAddon, true);
 });
